f9285c5fe8096d93e2b90512baf56d8469b5b9e9
Renato Moran <rveiga1992@gmail.com>
2020-06-01 15:27:39 -0300

    Delete 1013.js

--- 1013.js
+++
@@ -1,9 +0,0 @@
-var input = require("fs").readFileSync("stdin", "utf-8");
-var lines = input.split("\n");
-
-var x = parseInt(lines.shift());
-var y = parseFloat(lines.shift());
-
-var comsumo = x / y;
-
-console.log(comsumo.toFixed(3) + " km/l");
