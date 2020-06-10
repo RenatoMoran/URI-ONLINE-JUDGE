06a84eb713436ac3dee06bc0975f0742931f2b4f
Renato Moran <rveiga1992@gmail.com>
2020-06-10 00:22:56 -0300

    Add files via upload

---
+++ 1096.js
@@ -0,0 +1,20 @@
+var I = 1;
+var J = 8;
+
+while ((I != 9) || (J != 5)) {
+
+
+
+    if (J == 5) {
+        I = I + 2;
+        J = 7;
+    } else {
+
+        J--;
+
+    }
+
+    console.log("I=" + I + " J=" + J);
+
+
+}
---
+++ 1097.js
@@ -0,0 +1,7 @@
+var I, J, a = 7,
+    b;
+for (I = 1; I <= 9; I = I + 2) {
+    for (b = 1, J = a; b <= 3; J--, b++)
+        console.log("I=" + I + " J=" + J);
+    a = a + 2;
+}
